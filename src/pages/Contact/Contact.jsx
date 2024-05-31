c37b0c3809878132d4b675ba0ef392aa4b1d6b11
ariful007 <arifulislamwd007@gmail.com>
2024-05-31 21:00:33 +0600

    first commit

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,46 @@
+
+
+const Contact = () => {
+  return (
+    <section className="contact" data-page="contact">
+
+      <header>
+        <h2 className="h2 article-title">Contact</h2>
+      </header>
+
+      <section className="mapbox" data-mapbox>
+        <figure>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3642.0788328674444!2d90.43358024456133!3d24.09872301990009!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755d9c567761413%3A0x91c88aa54b225f2c!2sRajendrapur%20Cantonment%2C%20Gazipur!5e0!3m2!1sen!2sbd!4v1710412018864!5m2!1sen!2sbd"
+            width="400" height="300" loading="lazy" title="Google Map"></iframe>
+        </figure>
+      </section>
+
+      <section className="contact-form">
+
+        <h3 className="h3 form-title">Contact Form</h3>
+
+        <form action="#" className="form" data-form>
+
+          <div className="input-wrapper">
+            <input type="text" name="fullname" className="form-input" placeholder="Full name" required data-form-input />
+
+            <input type="email" name="email" className="form-input" placeholder="Email address" required data-form-input />
+          </div>
+
+          <textarea name="message" className="form-input" placeholder="Your Message" required data-form-input></textarea>
+
+          <button className="form-btn" type="submit" data-form-btn>
+            <ion-icon name="paper-plane"></ion-icon>
+            <span>Send Message</span>
+          </button>
+
+        </form>
+
+      </section>
+
+    </section>
+  );
+};
+
+export default Contact;
